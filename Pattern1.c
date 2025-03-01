0bb7860813cbde4d005a4c9082206f309b48d676
Homesh Dega <homeshdega06@gmail.com>
2025-03-01 10:21:14 +0530

    Update and rename README.md to Pattern1.c

---
+++ Pattern1.c
@@ -0,0 +1,22 @@
+
+output:
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+------------------------------
+#include <stdio.h>
+
+int pattern1() {
+    int n;
+    printf("Enter the rows:");
+    scanf("%d",&n);
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n;col++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
