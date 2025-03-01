90b0961b827798e35e43f55dbe34426bea417f2b
Homesh Dega <homeshdega06@gmail.com>
2025-03-01 10:25:12 +0530

    Create Pattern2.c

---
+++ Pattern2.c
@@ -0,0 +1,19 @@
+Output:
+*
+* *
+* * *
+* * * *
+--------Code--------
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the rows:");
+    scanf("%d",&n);
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=row;col++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
