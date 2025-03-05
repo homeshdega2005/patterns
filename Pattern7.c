7f954a7ab9170908c6016d3987b7986002981a36
Homesh Dega <homeshdega06@gmail.com>
2025-03-05 20:33:32 +0530

    Create Pattern7.c

---
+++ Pattern7.c
@@ -0,0 +1,32 @@
+Output:
+
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+
+------------Code------------
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the rows:");
+    scanf("%d",&n);
+    for(int row=0;row<2*n;row++){
+        int totalColInRow=row>n ? 2*n-row:row;
+        int noOfSpaces=n-totalColInRow;
+        for(int s=0;s<noOfSpaces;s++){
+            printf(" ");
+        }
+        for(int col=0;col<totalColInRow;col++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+      
